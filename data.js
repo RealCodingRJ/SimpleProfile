46fc47ec66f6bdae3e6752617da6412f1054cf95
RYAN JAMES <realcoder2023@gmail.com>
2025-11-20 16:25:42 -0500

    added imaGE CIRCEL AND ADDED EXPERIENCE TO CODE

--- data.js
+++ data.js
@@ -15,7 +15,14 @@ class App {
   }
 }
 
+function linkHeaderChange() {
+  const link = document.querySelectorAll("#projectsId");
+  return link;
+}
+
 function getData() {
+  const linksHeaderChnage = linkHeaderChange();
+
   const button = document.querySelectorAll("button");
   button.forEach((e) => {
     e.addEventListener("click", () => {
@@ -27,37 +34,48 @@ function getData() {
       const ul = document.getElementById("ul");
 
       const apps = app.getIKnow();
+
+      for (let i = 0; i < apps.length; i++) {
+        const list = document.getElementById("listsDesktop");
+        const li1 = document.createElement("li");
+        list.appendChild(li1);
+        li1.textContent = apps[i];
+
+        const date = document.getElementById("date");
+        date.textContent = app.getExperience();
+      }
+
       for (let i = 0; i < apps.length; i++) {
         const li = document.createElement("li");
+        ul.appendChild(li);
 
-        if (app.getIKnow()[0] == "React") {
-          const date = document.getElementById("date");
-
-          document.querySelectorAll("#img").forEach((e) => {
-            ul.appendChild(li);
-            e.addEventListener("click", () => {
-              li.textContent = apps[i];
-              li.addEventListener("click", () => {
-                li.style.border = "1px solid #101010";
-                // li.style.margin = "0.8rem";
-                li.style.width = `${880 / 2}px`;
-                li.style.textAlign = "center";
-
-                date.addEventListener("click", () => {
-                  const link = document.querySelectorAll("#projectsId");
-                  // link.style.display = displayNone();
-                  link.forEach((e) => {
-                    e.textContent = "https://github.com/RealCodingRJ";
-                    e.style.display = "block";
-                  });
-                });
-
-                date.textContent = app.getExperience();
-                li.style.backgroundColor = "red";
-              });
-            });
+        const img = document.getElementById("img");
+        const wrapper = document.getElementById("wrapper");
+        img.addEventListener("click", () => {
+          wrapper.style.backgroundColor = "white";
+          wrapper.style.width = 1600 / 2 + "px";
+          wrapper.style.margin = "0 auto";
+          li.textContent = `|  ${apps[i]}  |`;
+          // li.style.backgroundColor = "white";
+          wrapper.style.height = 2 * 350 + "px";
+          li.addEventListener("click", () => {
+            const main = document.getElementById("main");
+            li.textContent = `|  ${apps[i]}  |`;
+            li.style.border = "3px solid #101010";
           });
-        }
+
+          if (wrapper.style.display === "none") {
+            li.style.display = "flex";
+            wrapper.style.display = "block";
+            wrapper.style.backgroundColor = "white";
+          }
+
+          const date = document.getElementById("date");
+          date.addEventListener("click", (e) => {
+            e.preventDefault();
+            li.textContent = apps[i];
+          });
+        });
       }
     });
   });
